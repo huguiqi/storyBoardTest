92c3c75645502b468118c8c6ce545614f1dbd1e8
huguiqi <guiqi.hu@163.com>
2013-06-05 00:19:51 +0800

    add

---
+++ storyboardTest/IndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  IndexViewController.h
+//  storyboardTest
+//
+//  Created by huguiqi on 13-6-4.
+//  Copyright (c) 2013年 huguiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IndexViewController : UIViewController
+
+@end
---
+++ storyboardTest/IndexViewController.m
@@ -0,0 +1,48 @@
+//
+//  IndexViewController.m
+//  storyboardTest
+//
+//  Created by huguiqi on 13-6-4.
+//  Copyright (c) 2013年 huguiqi. All rights reserved.
+//
+
+#import "IndexViewController.h"
+#import "MacAddress.h"
+
+@interface IndexViewController ()
+
+-(IBAction)testMacAddress:sender;
+
+@end
+
+@implementation IndexViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+-(IBAction)testMacAddress:sender
+{
+    NSLog(@"current macAddress:%@",[MacAddress currentAddress]);
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:[NSString stringWithFormat:@"current address %@",[MacAddress currentAddress] ] delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+    [alertView show];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
