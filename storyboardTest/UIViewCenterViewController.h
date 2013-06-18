8ae509ad6e847e5d18b9ac8999ca68e1b1395b21
huguiqi <guiqi.hu@163.com>
2013-06-18 23:56:00 +0800

    aaa

---
+++ storyboardTest/UIViewCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewCenterViewController.h
+//  storyboardTest
+//
+//  Created by huguiqi on 13-6-18.
+//  Copyright (c) 2013年 huguiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewCenterViewController : UIViewController
+
+@end
