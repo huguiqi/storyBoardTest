142c76e91f9edc5420d97b6babc4564d8d49afed
huguiqi <guiqi.hu@163.com>
2013-08-26 10:04:21 +0800

    add

---
+++ storyboardTest/MapKitViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapKitViewController.h
+//  storyboardTest
+//
+//  Created by 胡 桂祁 on 8/23/13.
+//  Copyright (c) 2013 huguiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapKitViewController : UIViewController
+
+@end
---
+++ storyboardTest/MapKitViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapKitViewController.m
+//  storyboardTest
+//
+//  Created by 胡 桂祁 on 8/23/13.
+//  Copyright (c) 2013 huguiqi. All rights reserved.
+//
+
+#import "MapKitViewController.h"
+
+@interface MapKitViewController ()
+
+@end
+
+@implementation MapKitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
