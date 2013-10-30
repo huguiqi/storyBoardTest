7ccf54e6eee6da4e3fd7f5e76fe824ecfa7f7f40
huguiqi <guiqi.hu@163.com>
2013-10-30 13:00:29 +0800

    add

---
+++ storyboardTest/JJNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  JJNavigationBar.h
+//  storyboardTest
+//
+//  Created by 胡 桂祁 on 10/30/13.
+//  Copyright (c) 2013 huguiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJNavigationBar : UINavigationBar
+
+@end
---
+++ storyboardTest/JJNavigationBar.m
@@ -0,0 +1,30 @@
+//
+//  JJNavigationBar.m
+//  storyboardTest
+//
+//  Created by 胡 桂祁 on 10/30/13.
+//  Copyright (c) 2013 huguiqi. All rights reserved.
+//
+
+#import "JJNavigationBar.h"
+
+@implementation JJNavigationBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    
+}
+
+@end
