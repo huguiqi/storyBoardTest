ad6d6fe70ff97d9e2675803a950abb48432c3cc6
huguiqi <guiqi.hu@163.com>
2013-11-07 18:15:51 +0800

    add Float

---
+++ storyboardTest/FloatDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FloatDemoViewController.h
+//  storyboardTest
+//
+//  Created by 胡 桂祁 on 11/7/13.
+//  Copyright (c) 2013 huguiqi. All rights reserved.
+//
+
+#import "WFJJViewController.h"
+
+@interface FloatDemoViewController : WFJJViewController
+
+@end
---
+++ storyboardTest/FloatDemoViewController.m
@@ -0,0 +1,29 @@
+//
+//  FloatDemoViewController.m
+//  storyboardTest
+//
+//  Created by 胡 桂祁 on 11/7/13.
+//  Copyright (c) 2013 huguiqi. All rights reserved.
+//
+
+#import "FloatDemoViewController.h"
+
+@interface FloatDemoViewController ()
+
+@end
+
+@implementation FloatDemoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
